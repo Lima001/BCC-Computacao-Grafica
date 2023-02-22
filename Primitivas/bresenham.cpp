0374f39e843749ecbb8b0d215aeb667c1851999d
Lima001 <limaedugabriel@gmail.com>
2023-02-21 23:23:04 -0300

    Adição algoritmo Bresenham (Experimental)

---
+++ Primitivas/bresenham.cpp
@@ -0,0 +1,125 @@
+#include <SDL.h>
+#include <math.h>
+#include <iostream>
+
+#define HEIGHT 600
+#define WIDTH 800
+#define XI 0 
+#define YI 0
+#define XF 100
+#define YF -10
+#define TITLE "Bresenham"
+
+/* 
+    Implementado considerando os diferentes quadrantes e as
+    simetrias entre o plano xy;
+
+    Os pontos de limite do segmento de linha podem ser passados
+    em qualquer ordem - não é necessário, por exemplo, passar o ponto mais
+    à esquerda como inicial...
+*/
+void desenharLinha(SDL_Renderer* r, int xi, int yi, int xf, int yf){
+    int dx = std::abs(xf-xi), dy = std::abs(yf-yi);
+    int a, b, wa_shift, wb_shift, end, x=xi, y=yi;
+    int* wa;
+    int* wb;
+
+    if (dx > dy){
+        a = dx;
+        b = dy;
+        wa = &x;
+        wb = &y;
+        end = xf;
+
+        if (xi > xf)
+            wa_shift = -1;
+        else 
+            wa_shift = 1;
+
+        if (yi > yf)
+            wb_shift = -1;
+        else if (yi == yf)
+            wb_shift = 0;
+        else
+            wb_shift = 1;
+
+    }
+    else {
+        a = dy;
+        b = dx;
+        wa = &y;
+        wb = &x;
+        end = yf;
+
+        if (xi > xf)
+            wb_shift = -1;
+        else 
+            wb_shift = 1;
+
+        if (yi > yf)
+            wa_shift = -1;
+        else if (yi == yf)
+            wa_shift = 0;
+        else
+            wa_shift = 1;
+    }
+
+    int twoB = 2*b, twoBA = 2*(b-a);
+    int p = 2*b-a;
+    
+    SDL_RenderDrawPoint(r, x+WIDTH/2, HEIGHT/2-y);
+    
+    while (*wa != end){
+        *wa += wa_shift;
+        
+        if (p<0)
+            p += twoB;
+
+        else {
+            *wb += wb_shift;
+            p += twoBA;
+        }
+
+        SDL_RenderDrawPoint(r, x+WIDTH/2, HEIGHT/2-y);
+    }
+}
+
+
+int main(){
+
+    if (SDL_Init(SDL_INIT_EVERYTHING) < 0){
+        std::cout << "SDL Init Error - " << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Window* window = nullptr;
+    SDL_Renderer* renderer = nullptr;
+    SDL_Event event;
+    
+    window = SDL_CreateWindow(TITLE, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, 0);
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
+
+    while (1){
+
+        SDL_PollEvent(&event);
+        
+        if (event.type == SDL_QUIT)
+            break;
+        
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderClear(renderer);
+
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+        desenharLinha(renderer, XI, YI, XF, YF);
+
+        SDL_RenderPresent(renderer);
+
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
+}
